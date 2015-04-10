4e07c8c4765b308aad4361130449d086593a3524
amanpurohit <aman.purohit@openlabs.co.in>
2015-04-10 15:37:32 +0530

    hello world example

--- test.py
+++ test.py
@@ -5,3 +5,10 @@
     :copyright: (c) 2015 by Openlabs Technologies & Consulting (P) Limited
     :license: BSD, see LICENSE for more details.
 """
+from tryton.model import ModelView, ModelSQL, fields
+
+__all__ = ['Hello']
+
+class Hello(ModelSQL, ModelView):
+    "HelloWorld"
+    __name__ = "hello.hello"
